09c750912faa8469138d5b189ad6a5815494a789
plantenpot <laurensvanb@live.nl>
2022-06-30 17:18:50 +0200

    Update Projects Section

--- main.js
+++ main.js
@@ -19,3 +19,47 @@ function changeText()
 }
 
 changeText();
+
+let year2019 = document.getElementById('2019');
+let year2020 = document.getElementById('2020');
+let year2021 = document.getElementById('2021');
+let year2022 = document.getElementById('2022');
+
+year2019.style.display = 'none';
+year2020.style.display = 'none';
+year2021.style.display = 'none';
+
+function changeYear(year)
+{
+    document.getElementById('yearButton').innerHTML = year;
+
+    if (year == 2019)
+    {
+        year2020.style.display = 'none';
+        year2021.style.display = 'none';
+        year2022.style.display = 'none';
+
+        year2019.style.display = 'block';
+    } else if (year == 2020)
+    {
+        year2019.style.display = 'none';
+        year2021.style.display = 'none';
+        year2022.style.display = 'none';
+
+        year2020.style.display = 'block';
+    } else if (year == 2021)
+    {
+        year2019.style.display = 'none';
+        year2020.style.display = 'none';
+        year2022.style.display = 'none';
+
+        year2021.style.display = 'block';
+    } else if (year == 2022)
+    {
+        year2019.style.display = 'none';
+        year2020.style.display = 'none';
+        year2021.style.display = 'none';
+
+        year2022.style.display = 'block';
+    }
+}
